950a8dc9fa95b1cee7b7d3d20a64f8d79ebf7647
Devendra Barmate <mackdevilhell@gmail.com>
2019-02-22 01:12:36 +0530

    Added custom google search feature

--- get_base_url.py
+++ get_base_url.py
@@ -1,5 +1,6 @@
 import os
 os.environ['YOUTUBE'] = "https://www.googleapis.com/youtube/v3"
+os.environ['CUSTOM_SEARCH'] = "https://www.googleapis.com/customsearch/v1"
 os.environ['OXFORD'] = "https://od-api.oxforddictionaries.com/api/v1"
 
 def get_base_url(key):
