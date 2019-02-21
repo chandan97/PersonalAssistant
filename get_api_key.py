b90e6a34a84c7cd0ed381125f3bcacf2cdfc9e2a
Devendra Barmate <mackdevilhell@gmail.com>
2019-02-22 01:40:15 +0530

    Deleted get_api_key.py for credentials security

--- get_api_key.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ['YOUTUBE'] = ""  # Chandan - Google Api
-os.environ['OXFORD_APP_ID'] = ""  # Devin - Oxford app id
-os.environ['OXFORD_APP_KEY'] = ""  # Devin - Oxford app key
-os.environ['CUSTOM_SEARCH_ID'] = ""  # Devin - Google Custom Search ID
-os.environ['CUSTOM_SEARCH_KEY'] = ""  # Devin - Google Custom Search Api
-
-def get_api_key(key):
-    return os.environ.get(key)
